09f8eac6e433ca4e2f55c9dc3b1f855f9df54f9d
ian nyachiro <141264241+iano24@users.noreply.github.com>
2024-07-05 22:56:43 +0300

    Add files via upload

---
+++ MyFinalP/reveal.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", revealOnScroll);
+    revealOnScroll();
+});
+
+function revealOnScroll() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 50;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
